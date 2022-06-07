d37d49f85c2e24f55f390269be69c0e5a3a69da0
corruptware <corruptware@gmail.com>
2022-06-07 11:10:26 +1000

    init push

---
+++ src/Huelog/__init__.py
@@ -0,0 +1,12 @@
+from .huelog import *
+
+__all__ = ['bad', 'bg', 'black', 'blue', 'bold', 'cyan', 'good', 'green',
+           'grey', 'info', 'italic', 'lblue', 'lightblue', 'lcyan', 'lgreen',
+           'lightgreen', 'lpurple', 'lightpurple', 'lred', 'lightred','orange',
+           'purple', 'que', 'red', 'run','strike', 'under', 'white', 'yellow']
+
+__version__ = '0.0.0'
+
+if __name__ == '__main__':
+    print('All commands: ', __all__)
+    print('Current version: ', __version__)
